16c8f8036df1a6e7e704bd645a3281eb721da9bb
MatheusLucasAndrade <theuslucasandrade@gmail.com>
2023-10-09 23:16:52 -0300

    init front-end project

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import Sidenav from "../../components/Sidenav/Sidenav";
+import "./index.css";
+
+const Home = () => {
+  return (
+    <div className="grid container">
+      <Sidenav />
+      <div>bb</div>
+    </div>
+  );
+};
+
+export default Home;
